287161539c6e85cc163125b389564f3103775f4f
Josemar Marques da Silva <40063315+jmarqsilva@users.noreply.github.com>
2025-01-16 16:10:15 -0300

    new

---
+++ Script-103.sql
@@ -0,0 +1,24 @@
+-- Criar usuário com senha
+CREATE ROLE novo_usuario WITH LOGIN PASSWORD 'sua_senha_segura';
+
+-- Conceder permissões
+grant all on <tabela> to <usuario>
+
+-- tirar as permissões
+revoke select on <tabela> to <usuario>
+
+-- ALterar a senha
+ALTER USER nome_usuario WITH PASSWORD 'nova_senha';
+
+-- Copiar as permissões de um usuário para Outro
+DECLARE
+    obj record;
+BEGIN
+    FOR obj IN
+        SELECT 'GRANT ' || privilege_type || ' ON ' || table_schema || '.' || table_name || ' TO novo_usuario;'
+        FROM information_schema.role_table_grants
+        WHERE grantee = 'usuario_existente'
+    LOOP
+        EXECUTE obj;
+    END LOOP;
+END;
